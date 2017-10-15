11835e0977d5cfdee69e9ea6216a9b404db6a91e
Thanh Dang <thanhtrdang@gmail.com>
2017-10-15 18:16:48 +0700

    Change private data class to anonymous object for not, and, or

--- src/main/kotlin/io/i101/library/ddd/Specification.kt
+++ src/main/kotlin/io/i101/library/ddd/Specification.kt
@@ -3,25 +3,23 @@ package io.i101.library.ddd
 /**
  * A specification, as explained in the DDD book.
  *
- * Use !, AND, OR for readability
+ * Use !, AND, OR for readability.
+ * Override isSatisfiedBy only, never override !, AND, OR.
  */
 interface Specification<T> {
     infix fun isSatisfiedBy(obj: T): Boolean
 
-    infix fun AND(other: Specification<T>): Specification<T> = AndSpecification(this, other)
-    infix fun OR(other: Specification<T>): Specification<T> = OrSpecification(this, other)
+    infix fun AND(other: Specification<T>): Specification<T> = object: Specification<T> {
+        override fun isSatisfiedBy(obj: T): Boolean = this@Specification.isSatisfiedBy(obj) && other.isSatisfiedBy(obj)
+    }
+    infix fun OR(other: Specification<T>): Specification<T> = object: Specification<T> {
+        override fun isSatisfiedBy(obj: T): Boolean = this@Specification.isSatisfiedBy(obj) || other.isSatisfiedBy(obj)
+    }
 
-    operator fun not(): Specification<T> = NotSpecification(this)
+    operator fun not(): Specification<T> = object: Specification<T> {
+        override fun isSatisfiedBy(obj: T): Boolean = !this@Specification.isSatisfiedBy(obj)
+    }
+
     infix fun and(other: Specification<T>): Specification<T> = AND(other)
     infix fun or(other: Specification<T>): Specification<T> = OR(other)
-}
-
-private data class NotSpecification<T>(val other: Specification<T>): Specification<T> {
-    override fun isSatisfiedBy(obj: T) = !other.isSatisfiedBy(obj)
-}
-private data class AndSpecification<T>(val spec1: Specification<T>, val spec2: Specification<T>): Specification<T> {
-    override fun isSatisfiedBy(obj: T) = spec1.isSatisfiedBy(obj) && spec2.isSatisfiedBy(obj)
-}
-private data class OrSpecification<T>(val spec1: Specification<T>, val spec2: Specification<T>): Specification<T> {
-    override fun isSatisfiedBy(obj: T) = spec1.isSatisfiedBy(obj) || spec2.isSatisfiedBy(obj)
 }
